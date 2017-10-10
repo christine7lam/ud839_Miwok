05d1270b992ea6dc839aa69c50cdb12718942a17
Christine Lam <hl913h@att.com>
2017-10-10 13:53:29 -0700

    Commit changes so far

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,8 +1,60 @@
 package com.example.android.miwok;
+import android.content.Context;
+import android.view.View;
 
 /**
- * Created by hl913h on 10/9/17.
+ * Displays text to the user.
  */
-
 public class Word {
+
+    // String default translation
+    private String mDefaultTranslation;
+
+    // String Miwok translation
+    private String mMiwokTranslation;
+
+    /**
+     * Constructs a new TextView with initial values for text and text color.
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Sets the string value in the TextView.
+     *
+     * @param text is the updated string to be displayed.
+     */
+    public void setDefaultTranslation(String text) {
+        mDefaultTranslation = text;
+    }
+
+    /**
+     * Gets the string value in the TextView.
+     *
+     * @return current text in the TextView.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Sets the string value in the TextView.
+     *
+     * @param text is the updated string to be displayed.
+     */
+    public void setMiwokTranslation(String text) {
+        mMiwokTranslation = text;
+    }
+
+    /**
+     * Gets the string value in the TextView.
+     *
+     * @return current text in the TextView.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
 }
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,53 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hl913h on 10/9/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwork_txt_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_txt_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
